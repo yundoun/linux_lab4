6ab2bd78a99fe353cc8c0de493c2f8b34941185b
yundoun <dbsehdns12@gmail.com>
2023-12-07 17:53:58 +0900

    Docs : 주석 및 폴더 변경

--- 스레드로 메세지 방송하기/main.c
+++ 스레드로 메세지 방송하기/main.c
@@ -5,13 +5,13 @@
 
 #define MAX_CLIENTS 5
 
-// 공유 데이터 구조
+// 공유 데이터 구조체 정의
 typedef struct
 {
-  char message[256];
-  int new_message;
-  pthread_mutex_t mutex;
-  pthread_cond_t cond;
+  char message[256];     // 클라이언트가 보낼 메시지를 저장하는 문자열
+  int new_message;       // 새로운 메시지 여부를 나타내는 플래그 (0 또는 1)
+  pthread_mutex_t mutex; // 뮤텍스로 공유 데이터에 대한 동기화 제공
+  pthread_cond_t cond;   // 조건 변수로 쓰레드 간 상태 변경 및 대기 관리
 } SharedData;
 
 // 서버 쓰레드 함수
@@ -23,7 +23,7 @@ void *server_thread(void *arg)
   {
     pthread_mutex_lock(&shared->mutex);
 
-    // 새 메시지가 있을 때까지 대기
+    // 새 메시지가 도착할 때까지 대기
     while (!shared->new_message)
     {
       pthread_cond_wait(&shared->cond, &shared->mutex);
@@ -33,6 +33,7 @@ void *server_thread(void *arg)
     printf("Server broadcasting: %s\n", shared->message);
     shared->new_message = 0;
 
+    // 모든 클라이언트에게 메시지를 보냈음을 알림
     pthread_cond_broadcast(&shared->cond);
     pthread_mutex_unlock(&shared->mutex);
   }
@@ -54,12 +55,12 @@ void *client_thread(void *arg)
     sprintf(shared->message, "Message from client %d", client_id);
     shared->new_message = 1;
 
-    // 서버에게 메시지 알림
+    // 서버에게 메시지 전달
     pthread_cond_signal(&shared->cond);
 
     pthread_mutex_unlock(&shared->mutex);
 
-    // 잠시 대기
+    // 일시적으로 대기
     sleep(1);
   }
 
@@ -83,13 +84,14 @@ int main()
     pthread_create(&clients[i], NULL, client_thread, &shared);
   }
 
-  // 쓰레드들이 끝날 때까지 대기
+  // 쓰레드들이 종료될 때까지 대기
   pthread_join(server, NULL);
   for (int i = 0; i < MAX_CLIENTS; i++)
   {
     pthread_join(clients[i], NULL);
   }
 
+  // 뮤텍스와 조건 변수 정리
   pthread_mutex_destroy(&shared.mutex);
   pthread_cond_destroy(&shared.cond);
 
